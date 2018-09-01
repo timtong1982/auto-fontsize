69606929f49bd367244f4653ddbecff1eb025104
timtong1982 <mingtong@outlook.com>
2018-09-01 00:17:20 -0700

    update

--- demo/bootstrap.tsx
+++ demo/bootstrap.tsx
@@ -5,11 +5,17 @@ import { AutoFontSize } from '../src/AutoFontSize'
 const renderComponent = () => {
     console.log('rendering...');
     ReactDOM.render(
-        <AutoFontSize
-            text="The quick brown fox jumps over the lazy dog"
-            textSizeStep={2}
-            targetLines={1}
-        />,
+        <React.Fragment>
+            {/* <AutoFontSize
+                text="The quick brown fox jumps over the lazy dog"
+                textSizeStep={2}
+                targetLines={1}
+            /> */}
+            <AutoFontSize
+                text="The quick brown fox jumps over the lazy dog"
+                fontSizeMapping={[{ fontSize: 28, lineHeight: '31px' }, { fontSize: 16, lineHeight: '19px' }]}
+            />
+        </React.Fragment>,
         document.getElementById('mounthere')
     );
 };
